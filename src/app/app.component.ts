be2023ebe640000304f6da3ef5d0cd0e1093aac5
Ammar Haider <ammar.haider@mac3345.bjss.co.uk>
2018-02-23 12:58:42 +0000

    nav in menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, NavController, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  @ViewChild('myNav') nav: NavController;
+  rootPage: any = HomePage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menu: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.overlaysWebView(false);
       splashScreen.hide();
     });
+  }
+
+  goHome() {
+    this.menu.close();
+    this.nav.push(HomePage);
   }
   
 }
