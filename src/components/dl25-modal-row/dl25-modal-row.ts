fa55e13f123e516568c14ec47c13e4c676d26405
Ammar Haider <ammar.haider@Mac3345.local>
2018-02-25 18:41:19 +0000

    connects section data to modal

--- src/components/dl25-modal-row/dl25-modal-row.ts
+++ src/components/dl25-modal-row/dl25-modal-row.ts
@@ -6,16 +6,13 @@ import { Component, Input } from '@angular/core';
  * See https://angular.io/api/core/Component for more info on Angular
  * Components.
  */
-interface FaultSection {
-  title: string;
-}
 @Component({
   selector: 'dl25-modal-row',
   templateUrl: 'dl25-modal-row.html'
 })
 export class Dl25ModalRowComponent {
 
-  @Input() section: FaultSection;
+  @Input() section: string;
 
   constructor() {
   }
--- src/components/dl25-modal/dl25-modal.ts
+++ src/components/dl25-modal/dl25-modal.ts
@@ -1,26 +1,13 @@
 import { Component } from '@angular/core';
 import { NavParams, ViewController } from 'ionic-angular';
 
-/**
- * Generated class for the Dl25ModalComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
-interface FaultSection {
-  title: string;
-}
-interface FaultData {
-  title: string;
-  sections: FaultSection[];
-}
 @Component({
   selector: 'dl25-modal',
   templateUrl: 'dl25-modal.html'
 })
 export class Dl25ModalComponent {
 
-  faultData: FaultData;
+  faultData: any;
 
   constructor(public params: NavParams, public viewCtrl: ViewController) {
     this.faultData = params.get('faultData');
