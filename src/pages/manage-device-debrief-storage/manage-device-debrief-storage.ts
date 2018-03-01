8f06daef12c70ab09dc8266223b570e24c2d6b61
Ammar Haider <ammar.haider@Mac3345.local>
2018-03-01 14:30:48 +0000

    more typos

--- src/pages/manage-device-debrief-storage/manage-device-debrief-storage.ts
+++ src/pages/manage-device-debrief-storage/manage-device-debrief-storage.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
-  selector: 'page-mange-device-debrief-storage',
-  templateUrl: 'mange-device-debrief-storage.html',
+  selector: 'page-manage-device-debrief-storage',
+  templateUrl: 'manage-device-debrief-storage.html',
 })
 export class ManageDeviceDebriefStoragePage {
 
