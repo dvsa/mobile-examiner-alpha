dca02f02f618643b8c2d7c72eaa49401bf817653
Ammar Haider <ammar.haider@Mac3345.local>
2018-03-02 10:52:24 +0000

    refactors dashboard to use enum

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -5,6 +5,7 @@ import { LdtmModePage } from '../ldtm-mode/ldtm-mode';
 import { PolicyDataPage } from '../policy-data/policy-data';
 import { ManageDeviceDebriefStoragePage } from '../manage-device-debrief-storage/manage-device-debrief-storage';
 import { TrainerModePage } from '../trainer-mode/trainer-mode';
+import { DashboardComponentLinks } from './enums/dashboard-component-links';
 
 @Component({
   selector: 'page-dashboard',
@@ -23,11 +24,11 @@ export class DashboardPage {
   }
 
   goToPage(page: string) {
-    if (page === 'JournalPage') return this.navCtrl.push(JournalPage);
-    if (page === 'LdtmModePage') return this.navCtrl.push(LdtmModePage);
-    if (page === 'PolicyDataPage') return this.navCtrl.push(PolicyDataPage);
-    if (page === 'ManageDeviceDebriefStoragePage') return this.navCtrl.push(ManageDeviceDebriefStoragePage);
-    if (page === 'TrainerModePage') return this.navCtrl.push(TrainerModePage);
+    if (page === DashboardComponentLinks.JournalPage) return this.navCtrl.push(JournalPage);
+    if (page === DashboardComponentLinks.LdtmModePage) return this.navCtrl.push(LdtmModePage);
+    if (page === DashboardComponentLinks.PolicyDataPage) return this.navCtrl.push(PolicyDataPage);
+    if (page === DashboardComponentLinks.ManageDeviceDebriefStoragePage) return this.navCtrl.push(ManageDeviceDebriefStoragePage);
+    if (page === DashboardComponentLinks.TrainerModePage) return this.navCtrl.push(TrainerModePage);
   }
 
 }
---
+++ src/pages/dashboard/enums/dashboard-component-links.ts
@@ -0,0 +1,7 @@
+export enum DashboardComponentLinks {
+    JournalPage = 'JournalPage',
+    LdtmModePage = 'LdtmModePage',
+    PolicyDataPage = 'PolicyDataPage',
+    ManageDeviceDebriefStoragePage = 'ManageDeviceDebriefStoragePage',
+    TrainerModePage = 'TrainerModePage'
+}
