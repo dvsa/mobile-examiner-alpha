f502c1e6853f4c32970970e1ba4899afbaab5170
Alec Taylor <alec.taylor@bjss.com>
2018-03-13 12:17:22 +0000

    Mes 334 all on one common (#12)
    * reverse click and hold interaction. Hold to add fault, click to open modal
    
    * mutlitab cleanup, home page changes
    
    * add ids to form elements
    
    * Added S & D Buttons, changed recording hazard into two step process, added new component all-on-one-page-sub-element-hold-no-modal and HazardRecorderProvider
    
    * v1 - fixed tapping on form
    
    * v1- extract manœuvres
    
    * v1:removed commented form-sections
    
    * v3 - Extracted Maneuvers to one button
    
    * Locked screen orientation into portrait mode
    
    * Added insomnia plugin
    
    * guarded insomnia & orientation lock for run on browser
    
    * Fixed checking if run on real device or in browser
    
    * Added ECO & ETA actions into v1 & v3 prototype
    
    * v1:improved stylings
    
    *  - import redux store from working example
     - create new versions of buttons and report header to integrate with redux store provider
    
    * Update modal to use redux store and add logic around serious and dangerous faults
    
    * display S and D labels on elements
    
    * clean up button element constructor for getting data
    
    * just a small comment on the button elements
    
    * add text underlining for last fault marked
    
    * disable fault marking if serious or dangerous recorded
    
    * Apply Alec Redux stuff to prototype v1 (along with undo feature)
    
    * reposition totals scorecard
    
    * add missing section ids on form
    
    * Applied highlighted last selected element + changed syntax

--- src/components/all-on-one-form-sub-element-hold-no-modal/all-on-one-form-sub-element-hold-no-modal.ts
+++ src/components/all-on-one-form-sub-element-hold-no-modal/all-on-one-form-sub-element-hold-no-modal.ts
@@ -1,6 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { FaultsScorecardProvider } from '../../providers/faults-scorecard/faults-scorecard';
 import { HazardRecorderProvider } from '../../providers/hazard-recorder/hazard-recorder';
+import { NgRedux } from '@angular-redux/store';
+import { IFaultCounter } from '../button-element/button-element';
+import { Observable } from 'rxjs/Observable';
+import { FaultStoreProvider } from '../../providers/fault-store/fault-store';
 
 @Component({
   selector: 'all-on-one-form-sub-element-hold-no-modal',
@@ -8,25 +12,50 @@ import { HazardRecorderProvider } from '../../providers/hazard-recorder/hazard-r
 })
 export class AllOnOneFormSubElementHoldNoModalComponent {
 
-  @Input()
-  text: string;
+  @Input('section') section: string = '';
+  @Input() text: string;
+
   serious: boolean = false;
   dangerous: boolean = false;
-  counter: number = 0;
 
-  constructor(private faultsService: FaultsScorecardProvider, private hazardRecorderProvider: HazardRecorderProvider) {
+  faultCounter: number;
+  isLastFault: boolean
+
+  constructor(private faultsService: FaultsScorecardProvider,
+              private hazardRecorderProvider: HazardRecorderProvider,
+              private ngRedux: NgRedux<IFaultCounter>,
+              private faultStore: FaultStoreProvider) {
+        // this might not be the most performant way to do things,
+    // as it checks all the sections
+    this.ngRedux.select(state => Object.keys(state.faults)
+      .filter(key => key === this.section)
+      .map(sectionName => {
+        return state.faults[sectionName];
+      })).subscribe(data => {
+        if (data.length > 0) {
+          this.faultCounter = data[0].fault;
+          this.serious = !!data[0].serious;
+          this.dangerous = !!data[0].dangerous;
+        }
+      });
+
+      this.ngRedux.select(state => state.faults.lastFault)
+      .subscribe(data => this.isLastFault = (data && data.id === this.section));
   }
 
   addDrivingFault() {
-    this.counter++;
+    // prevent fault marking
+    if (this.dangerous || this.serious) return;
+
+    this.faultStore.addFault(this.section, 'fault');
     this.faultsService.addDrivingFault();
   }
 
-  recordHazard(){
-    if(this.hazardRecorderProvider.isDangerousRecordingEnabled) {
-      this.updateDangerous(); 
+  recordHazard() {
+    if (this.hazardRecorderProvider.isDangerousRecordingEnabled) {
+      this.updateDangerous();
       this.hazardRecorderProvider.disableRecording();
-    } else if(this.hazardRecorderProvider.isSeriousRecordingEnabled){
+    } else if (this.hazardRecorderProvider.isSeriousRecordingEnabled) {
       this.updateSerious();
       this.hazardRecorderProvider.disableRecording();
     }
@@ -34,13 +63,23 @@ export class AllOnOneFormSubElementHoldNoModalComponent {
 
   updateSerious() {
     this.serious = !this.serious;
-    if (!this.serious) return this.faultsService.removeSerious();
+    if (!this.serious) {
+      this.faultStore.removeFault(this.section, 'serious');
+      return this.faultsService.removeSerious();
+    }
+   
+    this.faultStore.addFault(this.section, 'serious');  
     return this.faultsService.addSerious();
   }
 
   updateDangerous() {
     this.dangerous = !this.dangerous;
-    if (!this.dangerous) return this.faultsService.removeDangerous();
+    if (!this.dangerous) {
+      this.faultStore.removeFault(this.section, 'dangerous')
+      return this.faultsService.removeDangerous();
+    }
+
+    this.faultStore.addFault(this.section, 'dangerous');
     return this.faultsService.addDangerous();
   }
 
