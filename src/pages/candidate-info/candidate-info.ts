763bb52b59d97c9fb1ac81c45e936b0df0803ff5
fredski02 <frederico02@hotmail.com>
2018-04-09 13:12:36 +0100

    first commit

--- src/pages/candidate-info/candidate-info.ts
+++ src/pages/candidate-info/candidate-info.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { DeclarationConsentPage } from '../declaration-consent/declaration-consent';
 import { PolicyDataPage } from '../policy-data/policy-data';
 import { Page } from 'ionic-angular/navigation/nav-util';
+import { SignaturePad } from 'angular2-signaturepad/signature-pad';
+import { AppConfigProvider } from '../../providers/app-config/app-config';
+import { PretestChecksPage } from '../pretest-checks/pretest-checks';
 
 @Component({
   selector: 'page-candidate-info',
@@ -11,6 +14,18 @@ import { Page } from 'ionic-angular/navigation/nav-util';
 export class CandidateInfoPage {
   declarationConsentPage: Page = DeclarationConsentPage;
   policyDataPage: Page = PolicyDataPage;
+  pretestChecksPage: Page = PretestChecksPage;
+  signature: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public configService: AppConfigProvider
+  ) {
+    this.signature = this.navParams.get('signature');
+  }
+
+  confirmIdentity() {
+    this.navCtrl.push(this.pretestChecksPage);
+  }
 }
