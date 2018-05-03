1c8544df7de475c2c77dc34cabbe218ea292ba74
Mathieu André Hendey <m@mathieuhendey.com>
2018-05-03 14:40:57 +0100

    Add RecordDebriefButton component

---
+++ src/components/record-debrief-button/record-debrief-button.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'record-debrief-button',
+  templateUrl: 'record-debrief-button.html'
+})
+export class RecordDebriefButtonComponent {
+  constructor() {}
+}
