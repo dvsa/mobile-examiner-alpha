6eff00f76754f714e6053e9f011f82a376296811
fredski02 <frederico02@hotmail.com>
2018-05-08 16:16:51 +0100

    fixes

--- src/components/play-pause-debrief-button/play-pause-debrief-button.ts
+++ src/components/play-pause-debrief-button/play-pause-debrief-button.ts
@@ -7,6 +7,7 @@ import { Component, Input } from '@angular/core';
 export class PlayPauseDebriefButtonComponent {
   @Input() buttonStatus: string;
   @Input() disabled: boolean;
+  @Input() clickEventHandler: () => void;
   constructor() {}
   ionViewWillEnter() {}
   ngOnInit() {}
